d2ff2a2582590a09a9d286125606a30f75b9d4e7
Kamil Kucharski <k.kucharski@itl.waw.pl>
2021-04-01 18:36:23 +0200

    Add our IP to allowed hosts

--- govermentUserAPI/settings.py
+++ govermentUserAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3*9u#ir=28_hwnoo+5wrjhec-9sss+qon+s%!(&4ug*$j76rgh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['40.112.78.100']
 
 
 # Application definition
