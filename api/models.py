86168a645233465a4193d72ddf2c798dffc5b699
Kamil Kucharski <k.kucharski@itl.waw.pl>
2021-04-07 21:41:24 +0200

    Email and phone field not required

--- api/models.py
+++ api/models.py
@@ -5,8 +5,8 @@ class Person(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=50)
     pesel = models.CharField(max_length=11)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=9)
+    email = models.EmailField(null=True, blank=True)
+    phone_number = models.CharField(max_length=9, null=True, blank=True)
 
     def __str__(self):
         return f"{self.pesel} - {self.name} {self.surname}"
